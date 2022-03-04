20f7bb74669d5f5be65a0d7c535c7462a34022ff
yosi90 <yosi121990@hotmail.es>
2022-03-04 21:28:11 +0000

    Arreglado login

--- plantillas/documento-declaracion.inc.php
+++ plantillas/documento-declaracion.inc.php
@@ -15,14 +15,13 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/blog/app/controlsesion.inc.php';
     }
     echo "<title>$titulo</title>";
     ?>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="<?php echo CSS; ?>/bootstrap.min.css" rel="stylesheet">
     <link href="<?php echo CSS; ?>/index.css" rel="stylesheet">
     <link href="<?php echo CSS; ?>/mediaQueris.css" rel="stylesheet">
     <link href="<?php echo CSS; ?>/material.css" rel="stylesheet">
     <script type="text/javascript" src="<?php echo JS; ?>/jquery.min.js"></script>
-    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
-    </script>
+    <script type="text/javascript" src="<?php echo JS; ?>/bootstrap.bundle.js"></script>
     <script type="text/javascript" src="<?php echo JS; ?>/index.js"></script>
     <!-- <script type="text/javascript">
         $(document).ready(function() {
@@ -39,6 +38,6 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/blog/app/controlsesion.inc.php';
 <body>
     <canvas id="sakura"></canvas>
     <?php
-    include_once 'navbar.inc.php';
+        include_once 'navbar.inc.php';
     ?>
     <div class="pater">
--- plantillas/navbar.inc.php
+++ plantillas/navbar.inc.php
@@ -24,15 +24,15 @@
                 if (controlSesion::sesion_iniciada()) {
                 ?>
                     <div class="d-flex align-items-center col-12 col-lg-auto me-lg-auto">
-                        <a href="#" class="btn btn-outline-light d-block me-2">
+                        <div class="dropdown text-end">
+                            <a href="#" class="btn link-pink text-decoration-none dropdown-toggle" id="ddu" data-bs-toggle="dropdown" aria-expanded="false">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-bounding-box" viewBox="0 0 16 16">
                                 <path d="M1.5 1a.5.5 0 0 0-.5.5v3a.5.5 0 0 1-1 0v-3A1.5 1.5 0 0 1 1.5 0h3a.5.5 0 0 1 0 1h-3zM11 .5a.5.5 0 0 1 .5-.5h3A1.5 1.5 0 0 1 16 1.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 1-.5-.5zM.5 11a.5.5 0 0 1 .5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 1 0 1h-3A1.5 1.5 0 0 1 0 14.5v-3a.5.5 0 0 1 .5-.5zm15 0a.5.5 0 0 1 .5.5v3a1.5 1.5 0 0 1-1.5 1.5h-3a.5.5 0 0 1 0-1h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 1 .5-.5z" />
                                 <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm8-9a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
                             </svg>
                             <?php echo ' ' . $_SESSION['nombre_usuario'] ?>
                         </a>
-                        <div class="dropdown text-end">
-                            <ul class="dropdown-menu text-small" aria-labelledby="ddu">
+                            <ul class="dropdown-menu dropdown-menu-dark text-small mt-3" aria-labelledby="ddu">
                                 <li><a class="dropdown-item" href="#">Perfil</a></li>
                                 <li><a class="dropdown-item" href="#">Opciones</a></li>
                                 <li>
@@ -45,9 +45,6 @@
                                 <li><a class="dropdown-item" href="<?php echo RUTA_LOGOUT ?>">Cerrar sesión</a></li>
                             </ul>
                         </div>
-                        <a href="#" class="link-pink text-decoration-none dropdown-toggle" id="ddu" data-bs-toggle="dropdown" aria-expanded="false">
-                            <img src="https://github.com/mdo.png" alt="mdo" class="rounded-circle" width="32" height="32">
-                        </a>
                     </div>
                 <?php
                 } else {
